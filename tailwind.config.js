7b08f1884c346569f1b5b745fa561e3ecf31379e
jobaerjubu <jobaerahmed4142@gmail.com>
2025-08-31 13:57:38 +0600

    added a fixt that was no show image

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'clifford': '#da373d',
+                'primary': '#E76F51',
+            },
+            fontFamily: {
+                manrope: ['Manrope', 'sans-serif'],
+            }
+        }
+    }
+}
